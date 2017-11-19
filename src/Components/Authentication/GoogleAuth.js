35f5e6e1971538654f1301f2e065ba4fc2646ed4
Theresa Wilkin <theresawilkin@yahoo.com>
2017-11-18 19:14:06 -0500

    calendar

--- src/Components/Authentication/GoogleAuth.js
+++ src/Components/Authentication/GoogleAuth.js
@@ -2,9 +2,9 @@ import hello from 'hellojs';
 import React from 'react';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log(1)
+
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-console.log(process.env)
+
 hello.init({
     google: GOOGLE_CLIENT_ID
 }, {
@@ -14,7 +14,7 @@ hello.init({
 hello.on('auth.login', function(auth) {
 
 	// Call user information, for the given network
-	hello(auth.network).api('https://ancient-oasis-46992.herokuapp.com/').then(function(r) {
+	hello(auth.network).api('calendar').then(function(r) {
 		// Inject it into the container
     console.log(r)
 	// 	var label = document.getElementById('profile_' + auth.network);
